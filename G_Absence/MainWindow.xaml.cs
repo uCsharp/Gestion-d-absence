3de5e255a0303ed30df40f804970c33216aa237f
salah eddib <salaheddib.55@gmail.com>
2021-01-22 09:30:22 +0100

    add read aprenant

--- G_Absence/MainWindow.xaml.cs
+++ G_Absence/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,303 @@ namespace G_Absence
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DateTime date;
+
+        User user;
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            user = User.login("salaheddib@gmail.com", "12345");
+
+            date = DateTime.Now.StartOfWeek(DayOfWeek.Saturday);
+
+            DateTime mondayOfLastWeek = DateTime.Now.AddDays(-(int)date.DayOfWeek - 6);
+
+            DateTime mondayOLastWeek = mondayOfLastWeek.AddDays(-(int)date.DayOfWeek - 6);
+
+            //MessageBox.Show(mondayOLastWeek.StartOfWeek(DayOfWeek.Thursday).ToShortDateString());
+
+
+            var aprenetList = user.GetAprenant(1);
+
+            aprenetList.ForEach(aprenant =>
+            {
+                
+                Button button = new Button();
+
+                button.Content = aprenant["firstname"]+ " " + aprenant["lastname"] ;
+
+                button.Style = FindResource("present") as Style;
+                button.FontSize = 18;
+                button.FontWeight = FontWeights.SemiBold;
+                button.Name = "aprenant";
+                button.Click += new RoutedEventHandler(Button_Click);
+                button.Tag = aprenant["id"];
+                aprenantStack.Children.Add(button);
+
+
+              
+                    var absanceList = user.GetAbsence(aprenant["id"]);
+
+                //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: lundi
+
+                    Button buttonL = new Button();
+
+                    buttonL.Style = FindResource("present") as Style;
+                    absanceList.ForEach(absence =>
+                    {
+                        if (absence["date"] == DateTime.Now.StartOfWeek(DayOfWeek.Monday).ToShortDateString())
+                        {
+                            
+
+                            if (absence["duration"] == "jour" && absence["is_justify"] == "1")
+                            {
+                                buttonL.Style = FindResource("justify") as Style;
+                            }
+                            else if (absence["duration"] == "demijour")
+                            {
+
+                                buttonL.Style = FindResource("demiJour") as Style;
+
+                            }
+                            else
+                            {
+
+                                buttonL.Style = FindResource("jour") as Style;
+
+                            }
+
+                        }
+
+                    });
+
+
+                    buttonL.Name = "lundi";
+                    buttonL.Click += new RoutedEventHandler(Button_Click);
+                    buttonL.Tag = aprenant["id"];
+                    lundiStack.Children.Add(buttonL);
+
+
+                //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: mardi
+
+                Button buttonMar = new Button();
+
+                buttonMar.Style = FindResource("present") as Style;
+                absanceList.ForEach(absence =>
+                {
+
+
+                    if (absence["date"] == DateTime.Now.StartOfWeek(DayOfWeek.Tuesday).ToString())
+                    {
+
+                       
+
+                        if (absence["duration"] == "jour" && absence["is_justify"] == "True" )
+                        {
+                            buttonMar.Style = FindResource("justify") as Style;
+                        }
+                        else if (absence["duration"] == "demijour")
+                        {
+
+                            buttonMar.Style = FindResource("demiJour") as Style;
+
+                        }
+                        else
+                        {
+
+                            buttonMar.Style = FindResource("jour") as Style;
+
+                        }
+
+                    }
+
+                });
+
+
+                buttonMar.Name = "mardi";
+                buttonMar.Click += new RoutedEventHandler(Button_Click);
+                buttonMar.Tag = aprenant["id"];
+                mardiStack.Children.Add(buttonMar);
+
+
+
+
+                //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: mercredi
+
+                Button buttonMer = new Button();
+
+                buttonMer.Style = FindResource("present") as Style;
+                absanceList.ForEach(absence =>
+                {
+
+
+
+                    if (absence["date"] == DateTime.Now.StartOfWeek(DayOfWeek.Wednesday).ToString())
+                    {
+                       
+
+                        if (absence["duration"] == "jour" && absence["is_justify"] == "1")
+                        {
+                            buttonMer.Style = FindResource("justify") as Style;
+                        }
+                        else if (absence["duration"] == "demijour")
+                        {
+
+                            buttonMer.Style = FindResource("demiJour") as Style;
+
+                        }
+                        else
+                        {
+
+                            buttonMer.Style = FindResource("jour") as Style;
+
+                        }
+
+                    }
+
+                });
+
+
+                buttonMer.Name = "mercredi";
+                buttonMer.Click += new RoutedEventHandler(Button_Click);
+                buttonMer.Tag = aprenant["id"];
+                mercrediStack.Children.Add(buttonMer);
+
+
+                //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: jeudi
+
+                Button buttonJ = new Button();
+
+                buttonJ.Style = FindResource("present") as Style;
+                absanceList.ForEach(absence =>
+                {
+
+
+
+                    if (absence["date"] == DateTime.Now.StartOfWeek(DayOfWeek.Thursday).ToString())
+                    {
+
+
+                        if (absence["duration"] == "jour" && absence["is_justify"] == "1")
+                        {
+                            buttonJ.Style = FindResource("justify") as Style;
+                        }
+                        else if (absence["duration"] == "demijour")
+                        {
+
+                            buttonJ.Style = FindResource("demiJour") as Style;
+
+                        }
+                        else
+                        {
+
+                            buttonJ.Style = FindResource("jour") as Style;
+
+                        }
+
+                    }
+
+                });
+
+
+                buttonJ.Name = "mercredi";
+                buttonJ.Click += new RoutedEventHandler(Button_Click);
+                buttonJ.Tag = aprenant["id"];
+                jeudiStack.Children.Add(buttonJ);
+
+
+                //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: vendredi
+
+
+                Button buttonV = new Button();
+
+                buttonV.Style = FindResource("present") as Style;
+                absanceList.ForEach(absence =>
+                {
+
+
+
+                    if (absence["date"] == DateTime.Now.StartOfWeek(DayOfWeek.Friday).ToString())
+                    {
+
+
+                        if (absence["duration"] == "jour" && absence["is_justify"] == "1")
+                        {
+                            buttonV.Style = FindResource("justify") as Style;
+                        }
+                        else if (absence["duration"] == "demijour")
+                        {
+
+                            buttonV.Style = FindResource("demiJour") as Style;
+
+                        }
+                        else
+                        {
+
+                            buttonV.Style = FindResource("jour") as Style;
+
+                        }
+
+                    }
+
+                });
+
+
+                buttonV.Name = "mercredi";
+                buttonV.Click += new RoutedEventHandler(Button_Click);
+                buttonV.Tag = aprenant["id"];
+                VendrediStack.Children.Add(buttonV);
+
+
+
+
+
+            });
+
+
+
         }
 
-     
+        private void Button_Click(object sender, RoutedEventArgs e )
+        {
+            MessageBox.Show(date.StartOfWeek(DayOfWeek.Monday).ToLongDateString());
+
+
+        }
+
+
+  
+    }
+
+    public static class DateTimeExtensions
+    {
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            
+            if(dt.DayOfWeek < startOfWeek)
+            {
+                int daysToAdd = ((int)startOfWeek - (int)dt.DayOfWeek + 7) % 7;
+                return dt.AddDays(daysToAdd);
+            }
+            else
+            {
+                int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+                return dt.AddDays(-1 * diff).Date;
+            }
+
+           
+        }
+
+        public static DateTime GetNextWeekday(DateTime start, DayOfWeek day)
+        {
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysToAdd = ((int)day - (int)start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
+        }
+
+
     }
 }
---
+++ G_Absence/User.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Absence
+{
+ 
+
+        class User
+        {
+            private int id;
+            private string firstname;
+            private string lastname;
+            private string email;
+            private string role;
+            private string classe;
+
+            public string Role { get => role; set => role = value; }
+
+            AdoNET Ado;
+
+            public User(string id, string firstname, string lastname, string email, string role, string classe)
+            {
+                this.id = int.Parse(id);
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.email = email;
+                this.Role = role;
+                this.classe = classe;
+                Ado = new AdoNET();
+            }
+
+            public static User login(string email, string password)
+            {
+                User user = null;
+
+                AdoNET Ado = new AdoNET();
+                Ado.Connection.Open();
+                Ado.Command.CommandText = "select * from users where email = @email and [password] = @password ";
+                Ado.Command.Connection = Ado.Connection;
+                Ado.Command.Parameters.AddWithValue("@email", email);
+                Ado.Command.Parameters.AddWithValue("@password", password);
+                Ado.Reader = Ado.Command.ExecuteReader();
+                if (Ado.Reader.HasRows)
+                {
+                    Ado.Reader.Read();
+                    user = new User(Ado.Reader["id"].ToString(), Ado.Reader["firstname"].ToString(), Ado.Reader["lastname"].ToString(), Ado.Reader["email"].ToString(), Ado.Reader["role"].ToString(), Ado.Reader["class"].ToString());
+
+                }
+
+                Ado.Connection.Close();
+
+
+                return user;
+
+
+            }
+
+
+            public bool InsertUser(string firstname, string lastname, string email, string role, string classe, string password)
+            {
+                if (this.role != "admin")
+                    return false;
+
+                Ado.Connection.Open();
+                Ado.Command.CommandText = "insert into users values ( @email , @password , @firstname , @lastname , @role , @class )";
+                Ado.Command.Connection = Ado.Connection;
+                Ado.Command.Parameters.AddWithValue("@email", email);
+                Ado.Command.Parameters.AddWithValue("@password", password);
+                Ado.Command.Parameters.AddWithValue("@firstname", firstname);
+                Ado.Command.Parameters.AddWithValue("@lastname", lastname);
+                Ado.Command.Parameters.AddWithValue("@role", role);
+                Ado.Command.Parameters.AddWithValue("@class", classe);
+                int numRow = Ado.Command.ExecuteNonQuery();
+                Ado.Connection.Close();
+
+            return numRow == 1;
+
+                
+
+
+
+            }
+
+
+            public List<Dictionary<string, string>> GetAprenant(int classe)
+            {
+
+            var aprenants = new List<Dictionary<string, string>>();
+
+            Ado.Connection.Open();
+            Ado.Command.CommandText = "select * from users where class = @class";
+            Ado.Command.Connection = Ado.Connection;
+            Ado.Command.Parameters.AddWithValue("@class", classe);
+            Ado.Reader = Ado.Command.ExecuteReader();
+            
+
+
+            if (Ado.Reader.HasRows)
+            {
+                while (Ado.Reader.Read())
+                {
+                    var dict = new Dictionary<string, string> { { "id", Ado.Reader["id"].ToString() }, { "firstname", Ado.Reader["firstname"].ToString() }, { "lastname", Ado.Reader["lastname"].ToString() } };
+
+                    aprenants.Add(dict);
+
+                }
+
+            }
+            Ado.Connection.Close();
+
+            return aprenants;
+
+
+
+        }
+
+        public List<Dictionary<string, string>> GetAbsence(string id)
+        {
+            var absence = new List<Dictionary<string, string>>();
+
+            Ado.Connection.Open();
+            Ado.Command.CommandText = "select * from  absence where user_id = " +id;
+            Ado.Command.Connection = Ado.Connection;
+            
+            Ado.Reader = Ado.Command.ExecuteReader();
+
+            if (Ado.Reader.HasRows)
+            {
+                while (Ado.Reader.Read())
+                {
+                    var dict = new Dictionary<string, string> { { "id", Ado.Reader["id"].ToString() }, { "date", Ado.Reader["date"].ToString() }, { "duration", Ado.Reader["duration"].ToString() } , { "is_justify", Ado.Reader["is_justify"].ToString() } };
+
+                    absence.Add(dict);
+
+                }
+
+            }
+
+            Ado.Connection.Close();
+
+
+            return absence;
+
+        }
+
+
+
+    }
+
+    
+}
